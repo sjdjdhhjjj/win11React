a7e5120911b47dd4b2b4648d63d32506a312253e
sjdjdhhjjj <sjdjdhhjjj@users.noreply.github.com>
2026-08-20 10:55:19 +0000

    deploy: c63f3d293027fc332a87f1839ccf6e8b852b9bbb

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/win11React/sw.js', { scope: '/win11React/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
